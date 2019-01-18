6b69a8c65b44f43159e16333c4117e800d7b669c
YUUYA KUDAMATSU <yoyokurabu@gmail.com>
2019-01-18 20:43:14 +0900

    first commit to github

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home I Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
 
