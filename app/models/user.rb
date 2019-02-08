a8070a10d5ca5ae4ccd21d18b496a834a46449aa
YUUYA KUDAMATSU <yoyokurabu@gmail.com>
2019-02-09 00:28:27 +0900

    secure new

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   validates :email, presence: true,length: { maximum: 255 },
    fomat: { with:VALID_EMAIL_REGEX },
    uniqueness:{ case_sensiteve:false }
+   has_secure_password
 end
