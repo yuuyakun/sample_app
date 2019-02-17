e62f7fe0afd5694cb3eeee544e4f64864bd39d59
YUUYA KUDAMATSU <yoyokurabu@gmail.com>
2019-02-17 20:40:58 +0900

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,5 +16,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
    assert_select 'div#<CSS id for error explanation'
    assert_select 'div.<CSS class for field with error>'
 
+   test "valid signup information" do
+     get signup_path
+     assert_differens "User.count",1 do
+       post users_path,params:{user:{name:"Example User",
+         email:"user@exanple.com",
+         password:"password"
+         password_confirmation:"password" }}
+   end
+   follow_redirect!
+   assert_template 'users/show'
+
  end
 end
