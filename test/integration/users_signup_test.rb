77ef983c355a1e8c913e5f46808b1ef501f9fc1f
YUUYA KUDAMATSU <yoyokurabu@gmail.com>
2019-03-04 21:43:30 +0900

    create signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -8,7 +8,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
    get signup_path
    assert_no_differens 'User.count' do
      post users_path,params: { user: { name:"",
-       email:"user@invalid",
+       email:"user@invalid",git status
        password:"foo",
        password_confirmation:"bar"}}
      end
@@ -16,7 +16,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
      assert_template 'users/new'
      assert_select 'div#<CSS id for error explanation'
      assert_select 'div.<CSS class for field with error>'
-
  end
 
 
@@ -27,7 +26,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
          email:"user@exanple.com",
          password: "password",
          password_confirmation: "password" }}
-       end
+   end
+
    follow_redirect!
    assert_template 'users/show'
    assert is_logged_in?
