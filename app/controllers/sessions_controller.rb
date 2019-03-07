f8784e60352b1c6e8811585d835ba58fa36710f3
YUUYA KUDAMATSU <yoyokurabu@gmail.com>
2019-03-07 21:46:01 +0900

    create test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,7 @@ class SessionsController < ApplicationController
     end
   end
 
+# ユーザーログアウト
   def destroy
     log_out
     redirect_to root_url
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -9,13 +9,25 @@ require 'test helper'
     end
 
 
-  test "login with invalid infomation" do
+  test "login with valid infomation followed by logout" do
     get login_path
-    assert_template 'sessions/new'
-    post login_path, params: { session:{ email:"",password:""}}
-    assert_template 'sessions/new'
-    assert_not flash.empty?
-    get root_path
-    assert flash.empty?
+    post login_path, params: { session:{ email:"",password:"password"}}
+
+    assert is_logged_in
+    assert_redirected_to @user
+    forrow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]",login_path,count: 0
+    assert_select "a[href=?]",logout_path
+    assert_select "a[href=?]",user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    forrow_redirect!
+    assert_select "a[href=?]",login_path
+    assert_select "a[href=?]",logout_path, count: 0
+    assert_select "a[href=?]",user_path(@user),count: 0
   end
+
+
 end
