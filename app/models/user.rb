e0191ddb90142417aeacc7788b5022f346216386
YUUYA KUDAMATSU <yoyokurabu@gmail.com>
2019-03-09 22:19:15 +0900

    remember_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,35 @@
 class User < ApplicationRecord
+  #アクセス可能な属性を生成
+  attr_accessor :remember_token
   before_save{ self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,length: { maximum: 255 },
    format: { with: VALID_EMAIL_REGEX },
-   uniqueness:{ case_sensitive:false }
+   uniqueness:{ case_sensitive: false }
    has_secure_password
-   validates :password,presence:true,length: {minimum:6}
+   validates :password, presence: true,length: { minimum:6 }
 
    def User.digest(string)
      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST:
       BCrypt::Engine.cost
      BCrypt::Password.create(string, cost: cost)
-
    end
 
+# ランダムなトークンを返す
+   def User.new_token
+     SecureRandom.urlsafe_base64
+   end
 
+# ユーザーをデータベースに記録する
+   def remember
+     self.remember_token = User.new_token
+     update_attribute(:remember_digest, User.digest(remember_token))
+   end
+
+# 渡されたトークンがダイジェストと一致したらtrueにする
+   def authenticated?(remember_token)
+     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+   end
 
 end
