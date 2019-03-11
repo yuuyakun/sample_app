232093f3497422171ff7787305ca406063c75798
YUUYA KUDAMATSU <yoyokurabu@gmail.com>
2019-03-11 21:51:21 +0900

    params session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user= User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : footer(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
 # ユーザーログアウト
   def destroy
   # ログイン中のみログアウトする
-    log_out if legged_in?
+    log_out if logged_in?
     redirect_to root_url
   end
 end
