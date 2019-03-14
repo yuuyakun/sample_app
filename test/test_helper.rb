83c14aa90236ad3143819210447d4d6fd9332d88
YUUYA KUDAMATSU <yoyokurabu@gmail.com>
2019-03-14 23:27:03 +0900

    test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,11 +10,16 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 
-# テストユーザーとしてログインする
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+# テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
 end
+
 
 class ActionDispatch::IntegrationTest
 
