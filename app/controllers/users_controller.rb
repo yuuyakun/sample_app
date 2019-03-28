a1db8a0c56b44bd720b49b1e0bc364955cdc3fcf
YUUYA KUDAMATSU <yoyokurabu@gmail.com>
2019-03-28 21:24:30 +0900

    mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,12 +15,12 @@ class UsersController < ApplicationController
     @user=User.new
   end
 
-  def creat   # 会員情報を作成するメソッド
+  def create   # 会員情報を作成するメソッド
     @user = User.new(user_params)
     if @user.save    # 新期ユーザーを作成する機能
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -29,8 +29,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
    end
 
    follow_redirect!
-   assert_template 'users/show'
-   assert is_logged_in?
+   # assert_template 'users/show'
+   # assert is_logged_in?
 
    end
 end
