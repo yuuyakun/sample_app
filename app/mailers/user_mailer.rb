470c165ccd04a962591b69f4b1685af9921648c2
YUUYA KUDAMATSU <yoyokurabu@gmail.com>
2019-04-01 22:10:05 +0900

    create password reset html

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.account_activation.subject
   #
-  def account_activation(user)
+  def account_activation(user) # ユーザー登録したか確認メール
     @user = user
     mail to: user.email, subject: "Account activation"  # メールの件名
   end
@@ -15,9 +15,8 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user) # パスワードリセットした時の確認メール
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # ブラウザを再起動した時にログインできる機能
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_dijest
   validates :name, presence: true, length: { maximum: 50 }
@@ -43,6 +43,16 @@ class User < ApplicationRecord
 
    def send_activation_email # 有効化メールを送信する
      UserMailer.account_activation(self).deliver_now
+   end
+
+   def create_reset_digest # パスワード再設定の属性を設定
+     self.reset_token = User.new_token
+     update_attribute(:reset_digest, User.digest(reset_token))
+     update_attribute(:reset_sent_at, Time.zone.now)
+   end
+
+   def send_password_reset_email # パスワード再設定のメールを送信
+     UserMailer.password_reset(self).deliver_now
    end
 
 private
