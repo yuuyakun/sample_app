8c2156121870e09fac3a59ccc88c558963060535
YUUYA KUDAMATSU <yoyokurabu@gmail.com>
2019-04-10 22:47:58 +0900

    fixture

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts 
   # ブラウザを再起動した時にログインできる機能
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -7,7 +7,7 @@ class MicropostTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:michael)
-    @micropost = Micropost.new(content: "Lorem ipsum",user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
@@ -19,4 +19,18 @@ class MicropostTest < ActiveSupport::TestCase
     assert_not @micropost.valid?
   end
 
+  test "content should be present" do
+    @micropost.content = "  "
+    assert_not @micropost.valid?
+  end
+
+  test "content should be at most 140 characters" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
+  end
+  
 end
